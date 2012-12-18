6f389243abedf70d1f8dc3e34dec6d0345c6d0bb
Alf Eaton <alf@hubmed.org>
2012-12-18 13:32:08 +0000

    fuzzy methods matching

--- plugins/sections/sections.js
+++ plugins/sections/sections.js
@@ -8,11 +8,17 @@ $(document).on("article.loaded", function(event, article){
 		references: null
 	};
 
-	var methods = article.find("h2").each(function() {
+	article.find("h2").each(function() {
 		var node = $(this);
+
 		var heading = node.text().toLowerCase();
+
+		if (!sections["methods"] && heading.match(/\bmethods\b/)) {
+			heading = "methods";
+		}
+
 		sections[heading] = node.closest("section");
-	})
+	});
 
 	if (sections["methods"] && sections["results"]) {
 		sections["results"].after(sections["methods"]);
