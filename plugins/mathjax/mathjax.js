7b3085e86fdedb074162553091d7323cd392390e
Alf Eaton <alf@hubmed.org>
2013-03-02 16:53:31 +0000

    Update mathjax.js

--- plugins/mathjax/mathjax.js
+++ plugins/mathjax/mathjax.js
@@ -2,6 +2,6 @@ $(document).on("article.ready", function(event, article){
 	var script = document.createElement("script");
 	script.setAttribute("type", "text/javascript");
 	script.text = "MathJax.Hub.Startup.onload()";
-	script.setAttribute("src", "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML");
+	script.setAttribute("src", "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=MML_HTMLorMML");
 	document.getElementsByTagName("head")[0].appendChild(script);
 });
